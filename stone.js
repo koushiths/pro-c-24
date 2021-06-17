42fcbc55dcf5a3e7aff6808a4dbb5b1e37149828
koushiths <81985668+koushiths@users.noreply.github.com>
2021-06-17 20:41:17 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,33 @@
+class Stone{
+	constructor(x,y,width,height)
+	{
+	var options = {
+        'density':1,
+        'friction': 0.9,
+        'restitution':0.8
+	}
+		
+		this.body=Bodies.rectangle(x, y, width, height, options)
+        this.width=width;
+        this.height=height;
+          
+
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var stonepos=this.body.position;
+            var angle=this.body.angle		
+			push()
+			translate(stonepos.x, stonepos.y);
+            rotate(angle)
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("black");
+            rect(0,0,this.width,this.height)
+			pop()
+	}
+
+}
